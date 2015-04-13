01c796d8bd45c4a5ea216be0c89b8bf3391e3a60
SiryjVyiko <dz@hivext.net>
2015-04-13 13:29:05 +0300

    Update reset_password.sh

--- jelastic/scripts/reset_password.sh
+++ jelastic/scripts/reset_password.sh
@@ -2,15 +2,10 @@
 
 export JAVA_HOME="/usr/java/default/";
 
-#
-# This is an example of reset password hook in Jelastic
-#
-
 #$J_OPENSHIFT_APP_ADM_USER        ;   Operate this variable for the username
 #$J_OPENSHIFT_APP_ADM_PASSWORD    ;   Use this varible for your password
 
 function _setPassword() {
-        JBOSS_HOME="/opt/repo/versions/8.2";
         $OPENSHIFT_WILDFLY_DIR/versions/8.2/bin/add-user.sh --user ${J_OPENSHIFT_APP_ADM_USER} --password ${J_OPENSHIFT_APP_ADM_PASSWORD} --silent --enable
 }
 
