d4f7f6e394763d9c93a4237f3f49558d3af79e68
SiryjVyiko <dz@hivext.net>
2015-04-18 11:25:13 +0300

    Update reset_password.sh

--- jelastic/scripts/reset_password.sh
+++ jelastic/scripts/reset_password.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+source /etc/jelastic/environment;
 export JAVA_HOME="/usr/java/default/";
 
 #$J_OPENSHIFT_APP_ADM_USER        ;   Operate this variable for the username
 #$J_OPENSHIFT_APP_ADM_PASSWORD    ;   Use this varible for your password
 
 function _setPassword() {
-        $OPENSHIFT_WILDFLY_DIR/versions/8.2/bin/add-user.sh --user ${J_OPENSHIFT_APP_ADM_USER} --password ${J_OPENSHIFT_APP_ADM_PASSWORD} --silent --enable
+        $OPENSHIFT_WILDFLY_DIR/versions/${Version}/bin/add-user.sh --user ${J_OPENSHIFT_APP_ADM_USER} --password ${J_OPENSHIFT_APP_ADM_PASSWORD} --silent --enable
 }
 
 
